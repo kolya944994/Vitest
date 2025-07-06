1419054df8e15277fc3f641295cdf20fb804fbe6
kolya944994 <kolya944.994@mail.ru>
2025-07-06 11:19:33 +0500

    vite3

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-	base: '/Vitest/', // <-- добавь эту строку
+	base: '/Vitest/',
 	plugins: [react()],
 	test: {
 		globals: true,
